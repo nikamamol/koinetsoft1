8bbdeffffcd02fffd68041672cd3e5d9edf62fed
amol patil <amolspatil018@gmail.com>
2024-07-26 15:04:39 +0530

    register user

--- src/userloginsignup/Register.jsx
+++ src/userloginsignup/Register.jsx
@@ -1,59 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
-import LogoImge1 from "../assets/koinetlogo.png"
-
+import axios from 'axios';
+import LogoImge1 from "../assets/koinetlogo.png";
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-    const submitForm = (event) => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        phone: '',
+        role: 'user', // Set default value for role
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const submitForm = async (event) => {
         event.preventDefault();
-        // Handle form submission here
-        console.log("Form submitted");
+        try {
+            const response = await axios.post('http://localhost:4000/user/signup', formData);
+            console.log("User registered successfully:", response.data);
+            navigate('/'); // Redirect to login page after successful registration
+        } catch (error) {
+            console.error("Error during signup:", error.response.data);
+            // Handle error (e.g., show an error message)
+        }
     };
 
     return (
         <Container fluid className='bgGradinet'>
             <Row>
-                <Col lg={12} className="d-flex justify-content-center align-items-center vh-100 ">
+                <Col lg={12} className="d-flex justify-content-center align-items-center vh-100">
                     <Col lg={6}>
-                        <Card className=" border-0 shadow p-5" style={{ backgroundColor: '#F8F4E1' }}>
+                        <Card className="border-0 shadow p-5" style={{ backgroundColor: '#F8F4E1' }}>
                             <div className='text-center'>
-                                <p >
-                                    <img src={LogoImge1} alt="" width={200} height={100} />
-                                    <p>Koinet Media Ites Pvt Ltd.</p>
-                                </p>
-                                <div>
-                                    <h4> Welcome to Koinet-Media! 👋 </h4>
-                                    <p>
-                                        Sign-up to your account and roll up your sleeves to work on the campaign!
-                                    </p>
-                                </div>
+                                <img src={LogoImge1} alt="" width={200} height={100} />
+                                <p>Koinet Media Ites Pvt Ltd.</p>
+                                <h4>Welcome to Koinet-Media! 👋</h4>
+                                <p>Sign-up to your account and roll up your sleeves to work on the campaign!</p>
                             </div>
                             <Form id="registrationForm" onSubmit={submitForm}>
                                 <Form.Group className="mb-2">
                                     <Row className="mb-2">
                                         <Col md={6}>
                                             <Form.Label htmlFor="firstName">First Name</Form.Label>
-                                            <Form.Control type="text" id="firstName" name="firstName" />
+                                            <Form.Control
+                                                type="text"
+                                                id="firstName"
+                                                name="firstName"
+                                                value={formData.firstName}
+                                                onChange={handleChange}
+                                            />
                                         </Col>
                                         <Col md={6}>
                                             <Form.Label htmlFor="lastName">Last Name</Form.Label>
-                                            <Form.Control type="text" id="lastName" name="lastName" />
+                                            <Form.Control
+                                                type="text"
+                                                id="lastName"
+                                                name="lastName"
+                                                value={formData.lastName}
+                                                onChange={handleChange}
+                                            />
                                         </Col>
                                     </Row>
                                     <Form.Label htmlFor="email">Email</Form.Label>
-                                    <Form.Control type="email" id="email" name="email" />
+                                    <Form.Control
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-2">
                                     <Form.Label htmlFor="password">Password</Form.Label>
-                                    <Form.Control type="password" id="password" name="password" />
+                                    <Form.Control
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-2">
                                     <Form.Label htmlFor="phone">Phone</Form.Label>
-                                    <Form.Control type="text" id="phone" name="phone" />
+                                    <Form.Control
+                                        type="text"
+                                        id="phone"
+                                        name="phone"
+                                        value={formData.phone}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-2">
                                     <Form.Label htmlFor="role">Role</Form.Label>
-                                    <Form.Control as="select" id="role" name="role" required>
+                                    <Form.Control
+                                        as="select"
+                                        id="role"
+                                        name="role"
+                                        value={formData.role}
+                                        onChange={handleChange}
+                                        required
+                                    >
                                         <option value="admin">Admin</option>
                                         <option value="user">User</option>
                                         <option value="supervisor">Supervisor</option>
@@ -64,7 +119,7 @@ const Register = () => {
                                         <option value="delivery">Delivery</option>
                                     </Form.Control>
                                 </Form.Group>
-                                <div className="g-recaptcha" data-sitekey="6Ld3OPYpAAAAAEFwTbe61yzBBwg07nogKwTY82iu"></div>
+                                <div className="g-recaptcha" data-sitekey="YOUR_SITE_KEY_HERE"></div>
                                 <div className="text-center mt-3">
                                     <Button type="submit" className="btn btn-danger w-50">
                                         Register Now
